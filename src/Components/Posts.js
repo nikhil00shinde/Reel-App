cd2f7c8770c2813718b5b707a75ccbeb5ba18fb1
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-12-14 23:39:18 +0530

    comment modal half completed

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -5,9 +5,39 @@ import { database } from "../firebase";
 import Video from "./Video";
 import "./Posts.css";
 import Like from "./Like";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions } from "@mui/material";
+import { makeStyles } from "@material-ui/core/styles";
 
 function Posts({ userData }) {
+	const useStyles = makeStyles({
+		modal: {
+			height: "100%",
+			width: "100%",
+		},
+	});
+	const classes = useStyles();
+
 	const [posts, setPosts] = useState(null);
+	const [open, setOpen] = useState(null);
+
+	const handleClickOpen = (id) => {
+		setOpen(id);
+	};
+
+	const handleClose = () => {
+		setOpen(null);
+	};
 
 	useEffect(() => {
 		let parr = [];
@@ -40,6 +70,92 @@ function Posts({ userData }) {
 										<h4>{userData.fullname}</h4>
 									</div>
 									<Like userData={userData} postData={post} />
+									<ChatBubbleIcon
+										className="chat-styling"
+										onClick={() => handleClickOpen(post.pId)}
+									/>
+									<Dialog
+										open={open == post.pId}
+										onClose={handleClose}
+										aria-labelledby="alert-dialog-title"
+										aria-describedby="alert-dialog-description"
+										fullWidth={true}
+										maxWidth="md"
+									>
+										<div className="modal-container">
+											<div className="video-modal">
+												<video autoplay={true} muted="muted" controls>
+													<source src={post.pUrl}/>
+												</video>
+											</div>
+											<div className="comment-modal">
+												<Card sx={{ maxWidth: 345 }}>
+													<CardActionArea>
+														<CardMedia
+															component="img"
+															height="140"
+															image="/static/images/cards/contemplative-reptile.jpg"
+															alt="green iguana"
+														/>
+														<CardContent>
+															<Typography
+																gutterBottom
+																variant="h5"
+																component="div"
+															>
+																Lizard
+															</Typography>
+															<Typography
+																variant="body2"
+																color="text.secondary"
+															>
+																Lizards are a widespread group of squamate
+																reptiles, with over 6,000 species, ranging
+																across all continents except Antarctica
+															</Typography>
+														</CardContent>
+													</CardActionArea>
+													<CardActions>
+														<Button size="small" color="primary">
+															Share
+														</Button>
+													</CardActions>
+												</Card>
+												<Card sx={{ maxWidth: 345 }}>
+													<CardActionArea>
+														<CardMedia
+															component="img"
+															height="140"
+															image="/static/images/cards/contemplative-reptile.jpg"
+															alt="green iguana"
+														/>
+														<CardContent>
+															<Typography
+																gutterBottom
+																variant="h5"
+																component="div"
+															>
+																Lizard
+															</Typography>
+															<Typography
+																variant="body2"
+																color="text.secondary"
+															>
+																Lizards are a widespread group of squamate
+																reptiles, with over 6,000 species, ranging
+																across all continents except Antarctica
+															</Typography>
+														</CardContent>
+													</CardActionArea>
+													<CardActions>
+														<Button size="small" color="primary">
+															Share
+														</Button>
+													</CardActions>
+												</Card>
+											</div>
+										</div>
+									</Dialog>
 								</div>
 							</React.Fragment>
 						);
